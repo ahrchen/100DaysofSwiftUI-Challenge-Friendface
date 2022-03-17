a4f2503c9151210fa0d9de770610c1952b39e9bc
Raymond Chen <ahrchen@gmail.com>
2022-03-16 19:05:07 -0700

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Raymond Chen on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
